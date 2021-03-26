28250e062158d48936e11cf35b7fbea313cdb9ba
drgnlrd <burhanhp5158@gmail.com>
2021-03-26 20:51:16 +0530

    layout-changes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import Weather from './components/Weather';
 
 function App() {
   return (
-    <Box w="100%" h="100%" minHeight="100vh" p={[3, 5, 10]} bgGradient="linear(to-r, #43cea2, #185a9d)" className="App">
+    // bgGradient="linear(to-r, #43cea2, #185a9d)"
+    <Box w="100%" h="100%" minHeight="100vh" p={[3, 10, 10]} className="App" >
       <Box className="main-card" w="100%" h="100%" minHeight="85vh" p={[2, 4, 8]} bg="rgba(255,255,255,0.55)" borderRadius="10px" opacity="0.75" border="1px solid rgba(255,255,255,0.5)" >
         <Weather />
       </Box>
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -7,10 +7,13 @@ import {
     InputGroup, 
     Button,
     Icon,
-    Center
+    Box,
+    Grid,
+    GridItem,
+    // Center
 } from '@chakra-ui/react';
 
-import { BsArrowRight } from 'react-icons/bs';
+import { BiCurrentLocation } from 'react-icons/bi';
 import axios from 'axios';
 import moment from 'moment';
 import _ from 'lodash';
@@ -43,6 +46,33 @@ class Weather extends React.Component{
                 data
             })
         })
+
+    }
+
+    handleLocation = () =>{
+        if("geolocation" in navigator){
+            navigator.geolocation.getCurrentPosition((position)=>{
+                console.log('position:', position);
+                console.log('latitude:', position.coords.latitude);
+                console.log('longitude:', position.coords.longitude);
+                let long = position.coords.longitude;
+                let lat = position.coords.latitude;
+
+                axios.get(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${long}&localityLanguage=en`)
+                .then(res=>{
+                    console.log('data',res.data);
+                    this.setState({
+                        city: res.data.city,
+                    })
+                })
+                .then(()=>{
+                    this.handleSubmit();
+                })
+                
+            })
+        }else{
+            alert('GeoLocation Not Available In The Browser')
+        }
     }
 
     handleSubmit = (event) =>{
@@ -69,45 +99,99 @@ class Weather extends React.Component{
         let data = this.state.data
         return(
             <div>
-                    
-                <InputGroup mb="5" size="sm" w={{base: "100%", md: "45%", lg: "25%"}} borderColor="black" onChange={this.handleChange}>
+                  
+                <InputGroup mb="5" size="sm" w={{base: "100%", md: "45%", lg: "25%"}} borderColor="black" onChange={this.handleChange} onBlur={this.handleSubmit} >
                     <Input
                         pr="4.5rem"
                         type="text"
-                        placeholder="Enter City"
+                        placeholder="Enter Your City"
                         id="city"
+                        borderRadius="5px"
                     />
                     
-                        <Button type="submit" colorScheme="teal" h="2rem" ml="3" size="md" onClick={this.handleSubmit}>
-                            <Icon as={BsArrowRight} w={8} h={8} color="blue.100" />
+                        <Button type="submit" colorScheme="teal" h="2rem" ml="3" size="md" onClick={this.handleLocation}>
+                            <Icon as={BiCurrentLocation} w={6} h={6} color="blue.100" />
                         </Button>
                     
                 </InputGroup>
+                <Grid 
+                    minH="65vh"
+                    templateColumns={["repeat(1, 1fr)","repeat(7, 1fr)","repeat(7, 1fr)"]}
+                    templateRows={["repeat(4, 0.2fr)","repeat(2, 1fr)","repeat(2, 1fr)"]}
+                    gap={4}
+                    
+                >
+                    <GridItem rowSpan={[1,2,2]} colSpan={[1,3,3]} >
+                        <Box p={[3,4,5]} bg="rgba(255,255,255,1)" opacity="0.8" borderRadius="5px" h="100%" boxShadow="lg" border="1px solid rgba(255,255,255,1)" >
+
+                            <Text fontWeight="600">{ moment().format('h:mm a ,MMMM Do YYYY') }</Text>
+                            <Text fontSize="20px">Weather Forecast For <b>{this.state.city}</b> </Text>
                 
-                <Text fontSize="20px">Weather Forecast For <b>{this.state.city}</b> </Text>
-                <Text fontWeight="600">{ moment().format('h:mm a ,MMMM Do YYYY') }</Text>
-                <br/>
-                <Text>Temperature:<b> {data.main.temp}</b> &#8451;</Text>
-                {/* <h5>Min-Temperature: {data.main.temp_min}&#8451;</h5>
-                <h5>Max-Temperature: {data.main.temp_max}&#8451;</h5> */}
-                <Text>Pressure:<b> {data.main.pressure}</b> Pa</Text>
-                <Text>Humidity:<b> {data.main.humidity}</b> g/m<sup>3</sup></Text>
-                <Text>Wind-Speed:<b> {data.wind.speed}</b> km/j</Text>
-                <br/>
-                { data && data.weather.length > 0 && data.weather.map(elm=>{
-                    return(
-                        <div key={elm.id} className>
-                            <Text fontSize="17px">
-                                Weather: <b>{_.capitalize(elm.description)}</b>
-                            </Text>
-                            <Center>
-                                <img src={`http://openweathermap.org/img/wn/${elm.icon}@2x.png`} alt="" height="120px" width="120px" />
-                            </Center>
-                            
-                        </div>
+                            <br/>
+                            <Text>Temperature:<b> {data.main.temp}</b> &#8451;</Text>
+                            {/* <h5>Min-Temperature: {data.main.temp_min}&#8451;</h5>
+                            <h5>Max-Temperature: {data.main.temp_max}&#8451;</h5> */}
+                            <Text>Pressure:<b> {data.main.pressure}</b> Pa</Text>
+                            <Text>Humidity:<b> {data.main.humidity}</b> g/m<sup>3</sup></Text>
+                            <Text>Wind-Speed:<b> {data.wind.speed}</b> km/j</Text>
+                            <br/>
+                            { data && data.weather.length > 0 && data.weather.map(elm=>{
+                                return(
+                                    <div key={elm.id}>
+                                        <Text fontSize="17px">
+                                            Weather: <b>{_.capitalize(elm.description)}</b>
+                                        </Text>
+                                        <img src={`http://openweathermap.org/img/wn/${elm.icon}@2x.png`} alt="" height="120px" width="120px" />
+                                        
+                                    </div>
+                                    
+                                )
+                            })}
+
+                        </Box> 
+                    </GridItem>
+                    
+                    <GridItem rowSpan={[1,1,1]} colSpan={[1,4,4]} >
+                        <Box p={[3,4,5]} bg="rgba(255,255,255,1)" opacity="0.8" borderRadius="5px" h="100%" boxShadow="lg" >
+                    
+                        </Box> 
+                    </GridItem>
+                    
+                    <GridItem rowSpan={[2,1,1]} colSpan={[1,4,4]} >
+                        <Grid
+                        h="100%"
+                        templateColumns={["repeat(2, 1fr)","repeat(4, 1fr)","repeat(4, 1fr)"]}
+                        templateRows={["repeat(2, 1fr)","repeat(1, 1fr)","repeat(1, 1fr)"]}
+                        gridTemplateRows="auto"
+                        gap={4}
+                        >
+                            <GridItem rowSpan={[1,1,1]} colSpan={[1,1,1]} >
+                                <Box p={[3,4,5]} bg="rgba(255,255,255,1)" opacity="0.8" borderRadius="5px" h="100%" boxShadow="lg" >
                         
-                    )
-                })}
+                                </Box> 
+                            </GridItem>
+                            <GridItem rowSpan={[1,1,1]} colSpan={[1,1,1]} >
+                                <Box p={[3,4,5]} bg="rgba(255,255,255,1)" opacity="0.8" borderRadius="5px" h="100%" boxShadow="lg" >
+                        
+                                </Box> 
+                            </GridItem>
+                            <GridItem rowSpan={[1,1,1]} colSpan={[1,1,1]} >
+                                <Box p={[3,4,5]} bg="rgba(255,255,255,1)" opacity="0.8" borderRadius="5px" h="100%" boxShadow="lg" >
+                        
+                                </Box> 
+                            </GridItem>
+                            <GridItem rowSpan={[1,1,1]} colSpan={[1,1,1]} >
+                                <Box p={[3,4,5]} bg="rgba(255,255,255,1)" opacity="0.8" borderRadius="5px" h="100%" boxShadow="lg" >
+                        
+                                </Box> 
+                            </GridItem>
+                        </Grid>
+                        
+                    </GridItem>
+                </Grid>
+                 
+                
+                
                 
             </div>
         )
